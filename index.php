ffa91858296f7fc4491f378fb1b8d8dee40b29e7
arafatkazi19 <arafatkazi47@gmail.com>
2020-08-16 01:36:17 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Time Management</title>
+</head>
+<body>
+
+<h1 style="color:lawngreen;text-align: center">Employee Time Management</h1>
+
+<h3 style="color: red">Login</h3>
+<a href="login.php">Login Here</a>
+<p>Don't have an account <a href="admin_reg.php">Register Here</a></p>
+
+<p>Admin will provide username and password to the user</p>
+<p>Please Admin provide user this link "http://localhost/employee/login_user.php" for login</p>
+
+</body>
+</html>
